4f696d0d9f9af7b569e8429d1b69583c800d34a6
redice <redice@warmachine.localdomain>
2025-10-03 02:19:27 +0300

    Base storage

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -3,13 +3,17 @@ package storage
 import "context"
 
 // Storage is the interface that wraps the User method.
-type Storage interface {
-	BeginTx(ctx context.Context) (TxStorage, error)
+type BaseStorage interface {
 	User() UserRepository
 }
 
+type Storage interface {
+	BaseStorage
+	BeginTx(ctx context.Context) (TxStorage, error)
+}
+
 type TxStorage interface {
+	BaseStorage
 	Commit() error
 	Rollback() error
-	User() UserRepository
 }
